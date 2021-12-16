100759d801156ac22614b10f64ab2d59db2d8f9a
agnesrausz <rauszka94@gmail.com>
2021-12-16 22:44:09 +0100

    Create Search.js

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div>
+            <form >
+                <input type="search"/>
+                <button type="button">Search</button>
+            </form>
+        </div>
+    );
+}
+
+export default Search;
