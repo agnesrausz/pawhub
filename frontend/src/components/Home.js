591ab4415ed88a0b7c7e1e87d2abafa6bb64af11
agnesrausz <rauszka94@gmail.com>
2021-12-16 22:52:58 +0100

    .

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import PetCard from "./PetCard";
 
 function Home(props) {
     const [pets, setPets] = useState([])
@@ -8,15 +9,33 @@ function Home(props) {
         fetchPets();
     }, [])
 
-    const fetchPets = async (url) => {
+    const fetchPets = async () => {
         const pets = await axios.get('http://localhost:8000/api/pets');
-        console.log(pets.data.pets[0].name)
+        // console.log(pets.data.pets[0])
+        // console.log(pets.data.pets.length)
+        let petList = pets.data.pets
+
+        for (let i = 0; i < petList.length; i++) {
+            if (pets.length !== 0) {
+                setPets(state => [...state, petList[i]])
+            }
+        }
     }
+
 
     return (
         <div>
-            <h1>Home</h1>
-            {localStorage.getItem('authToken') ? <p>hello user</p>:<p>hello stranger</p>}
+            <div className='title'>Pets</div>
+            {localStorage.getItem('authToken') ? <p>hello user</p> : <p>hello stranger</p>}
+            <div className='pet-list'>
+                {pets.map((pet) => (
+                    <div key={pet.id}>
+                        <PetCard pet={pet}/>
+                    </div>
+                ))}
+            </div>
+            <br/>
+
         </div>
     );
 }
