906f305fc23887e84bf260cb156e2e6dc346664c
agnesrausz <rauszka94@gmail.com>
2021-12-16 22:45:57 +0100

    Implement getPet, addPet methods

--- backend/app/Http/Controllers/PetController.php
+++ backend/app/Http/Controllers/PetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Pet;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PetController extends Controller
 {
@@ -17,7 +18,53 @@ class PetController extends Controller
         } else {
             return response([
                 "pets" => $pets
-            ], 200);
+            ], Response::HTTP_OK);
         }
+    }
+
+    public function getPet(Request $request)
+    {
+        $petId = request('id');
+        $pet = Pet::find($petId);
+
+        if(!$pet){
+            return response([
+                "message" => "Pet not found!"
+            ],404);
+        } else {
+            return response([
+                "pet" => $pet
+            ],200);
+        }
+    }
+
+    public function addPet(Request $request){
+        $fields = $request->validate([
+            'user_id' => 'numeric',
+            'name' => 'string',
+            'status' => 'string',
+            'sex' => 'string',
+            'profile_picture'=> 'string',
+            'species' => 'string',
+            'breed' => 'string',
+            'birth_date' => 'date'
+        ]);
+
+        $pet =Pet::create([
+            "user_id" => $fields['user_id'],
+            "name" => $fields['name'],
+            "status" => $fields['status'],
+            "sex" => $fields['sex'],
+            "profile_picture" => $fields['profile_picture'],
+            "species" => $fields['species'],
+            "breed" => $fields['breed'],
+            "birth_date" => $fields['birth_date'],
+        ]);
+
+        $response = [
+            "pet"  => $pet,
+        ];
+
+        return response($response,201);
     }
 }
